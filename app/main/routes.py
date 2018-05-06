6d39a47e63321ad762ad09b94a3ee3709e7ea5f1
Carlos <carlos.w.montecinos@gmail.com>
2018-05-06 14:20:19 -0300

    Blog Pagination

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request, current_app
 from flask_login import login_required, current_user
 from app.main import bp
 from app.models import User, Post
@@ -13,9 +13,19 @@ def index():
 
 @bp.route('/blog')
 def blog():
-    all_posts = Post.query.order_by(Post.timestamp.desc()).all()
+    # pagination
+    page = request.args.get('page', 1, type=int)
+    all_posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'])
+    # get the next page url
+    next_url = url_for('main.blog', page=all_posts.next_num) \
+        if all_posts.has_next else None
+    # get the previous page url
+    prev_url = url_for('main.blog', page=all_posts.prev_num) \
+        if all_posts.has_prev else None
     return render_template('main/blog.html', title='Blog', all_posts=all_posts,
-                           blog_active='is-active')
+                           blog_active='is-active', next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/article/<id>')
