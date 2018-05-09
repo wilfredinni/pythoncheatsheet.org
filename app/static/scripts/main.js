719ce461fed61efcf877bef4d25e4dcf4bd8f054
Carlos <carlos.w.montecinos@gmail.com>
2018-05-09 19:07:51 -0300

    Added Modal to delete Articles

---
+++ app/static/scripts/main.js
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------------------
+//                                DELETE MODAL
+// ----------------------------------------------------------------------------
+// get deleteModal, deleteModalBtn and closeBrn by id
+let deleteModal = document.getElementById('deleteModal');
+let deleteModalBtns = document.getElementsByClassName('deleteModalBtn');
+let closeBtn = document.getElementById('closeBtn');
+let cancel = document.getElementById('cancel');
+
+// arrows functions to open and close the modals
+const openModal = () => deleteModal.classList.add('is-active');
+const closeModal = () => deleteModal.classList.remove('is-active');
+
+// listen every delete button from overview and manage articles
+for (let button of deleteModalBtns) {
+    button.addEventListener('click', openModal);
+}
+
+// listen for clicks to close the modal
+closeBtn.addEventListener('click', closeModal);
+modalBackground.addEventListener('click', closeModal);
+cancel.addEventListener('click', closeModal);
+
+// ----------------------------------------------------------------------------
