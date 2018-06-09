e18e4444a43105ed54e55ca5760313062c71c304
Carlos <carlos.w.montecinos@gmail.com>
2018-06-09 19:55:59 -0400

    Index Notification Modal

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -3,15 +3,20 @@ let deleteModal = document.getElementById("deleteModal");
 let deleteModalBtns = document.getElementsByClassName("deleteModalBtn");
 let closeBtn = document.getElementById("closeBtn");
 let cancel = document.getElementById("cancel");
+let closeNotificationButton = document.getElementById("close-notification");
+let notification = document.getElementById("close-notification");
 
 // arrows functions to open and close the modals
 const openModal = () => deleteModal.classList.add("is-active");
 const closeModal = () => deleteModal.classList.remove("is-active");
+const closeNoti = () => notification.style.display = "none";
 
 // listen every delete button from overview and manage articles
 for (let button of deleteModalBtns) {
   button.addEventListener("click", openModal);
 }
+
+closeNotificationButton.addEventListener("click", closeNoti);
 
 // listen for clicks to close the modal
 if (closeBtn) {
