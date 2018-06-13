e191ce11f152a3d5ee4be1f94015e6dbf028912c
Carlos <carlos.w.montecinos@gmail.com>
2018-06-13 15:43:15 -0400

    Post body

--- app/models.py
+++ app/models.py
@@ -111,7 +111,7 @@ class Post(SearchableMixin, db.Model):
     __searchable__ = ['title', 'body']
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(280), index=True, unique=True)
-    body = db.Column(db.String(1000000))
+    body = db.Column(db.String(max))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     # many-to-many tags-posts
