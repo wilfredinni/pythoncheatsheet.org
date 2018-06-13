6f0c838a183d936a28d2fdd7f15086fcd642cdf4
Carlos <carlos.w.montecinos@gmail.com>
2018-06-13 17:59:42 -0400

    init.py pysheet

--- app/__init__.py
+++ app/__init__.py
@@ -59,7 +59,7 @@ def create_app(config_class=Config):
             mail_handler = SMTPHandler(
                 mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
                 fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                toaddrs=app.config['ADMINS'], subject='Pysheet Failure',
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
@@ -75,7 +75,7 @@ def create_app(config_class=Config):
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.logger.info('Pysheet startup')
 
     return app
 
