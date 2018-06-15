06f26e5cd2a0808d6992e9207f902d096055c41e
Carlos <carlos.w.montecinos@gmail.com>
2018-06-15 17:26:48 -0400

    pin msg 500 err

--- app/models.py
+++ app/models.py
@@ -145,7 +145,7 @@ class Tag(db.Model):
 
 class PinedMsg(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    home_msg = db.Column(db.String(4000), index=True, unique=True)
+    home_msg = db.Column(db.String(3000), index=True, unique=True)
 
 
 @login.user_loader
