7c397d8ebaf5446f6263dcc7f418f3af36c7d63d
Carlos <carlos.w.montecinos@gmail.com>
2018-09-23 15:40:33 -0300

    Acme

--- app/main/routes.py
+++ app/main/routes.py
@@ -130,3 +130,11 @@ def robot():
 @bp.route('/BingSiteAuth.xml')
 def bing():
     return send_from_directory('static', filename='BingSiteAuth.xml')
+
+@bp.route('/a0BJF3iQ6xqoDqmDh0BzVv3LKjFsGSLGKVOyVHBKheE')
+def acme1():
+    return send_from_directory('static/.well-known/acme-challenge', filename='a0BJF3iQ6xqoDqmDh0BzVv3LKjFsGSLGKVOyVHBKheE')
+
+@bp.route('/YGdnJ05dNgYYVYkWfDNHFTBJ9jVeMm4WDl36FxCi2gk')
+def acme2():
+    return send_from_directory('static/.well-known/acme-challenge', filename='YGdnJ05dNgYYVYkWfDNHFTBJ9jVeMm4WDl36FxCi2gk')
